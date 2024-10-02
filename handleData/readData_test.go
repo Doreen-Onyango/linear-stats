de1488c5c7ddba1fa509429e7a5332ba417c6929
doonyango <datieno001@gmail.com>
2024-10-02 16:43:14 +0300

    tests: add test for index data function

--- handleData/readData_test.go
+++ handleData/readData_test.go
@@ -35,3 +35,33 @@ func TestReadData(t *testing.T) {
 		})
 	}
 }
+
+func TestIndexData(t *testing.T) {
+	type args struct {
+		data  []float64
+		value float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Value does not exist",
+			args: args{data: []float64{1.0, 2.5, 3.0, 4.5, 5.0}, value: 6.0},
+			want: -1,
+		},
+		{
+			name: "Duplicate values",
+			args: args{data: []float64{1.0, 2.5, 3.0, 2.5, 5.0}, value: 2.5},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IndexData(tt.args.data, tt.args.value); got != tt.want {
+				t.Errorf("IndexData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
